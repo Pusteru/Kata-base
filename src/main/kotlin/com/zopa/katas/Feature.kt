0e7250d94bac473c7063bce45be776e212c1d9c9
javier.salinas <javier.salinas@zopa.com>
2020-05-27 14:38:28 +0200

    first iteration

--- src/main/kotlin/com/zopa/katas/Feature.kt
+++ src/main/kotlin/com/zopa/katas/Feature.kt
@@ -2,8 +2,19 @@ package com.zopa.katas
 
 
 fun main() {
+  var sum = 0
   while (true) {
-    val result = readLine()
-    println(result)
+    val result = readLine()!!
+    sum += toSum(result)
+    println(sum)
   }
 }
+fun toSum(fruit: String): Int {
+  return when (fruit) {
+    "Apple" -> 100
+    "Cherry" -> 70
+    "Banana" -> 130
+    else -> 0
+  }
+}
+
