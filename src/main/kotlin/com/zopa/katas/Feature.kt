8f2f864276da4870a341640af3180ef235e92775
javier.salinas <javier.salinas@zopa.com>
2020-05-27 15:12:25 +0200

    4 iteration

--- src/main/kotlin/com/zopa/katas/Feature.kt
+++ src/main/kotlin/com/zopa/katas/Feature.kt
@@ -1,45 +1,44 @@
 package com.zopa.katas
 
 import com.zopa.katas.Fruit.*
-import sun.invoke.empty.Empty
-
 
 enum class Fruit(val price: Int, val discount: Int) {
   Apple(100, 0),
-  Cherry(70, 20),
-  Banana(130, 0),
+  Cherry(70, 30),
+  Banana(130, 130),
   Empty(0, 0)
 }
 
 fun main() {
+  val listOfFruits = mutableListOf<Fruit>()
   while (true) {
     val result = readLine()!!
-    println(calculateSum(parseLine(result)))
+    listOfFruits.addAll(parseLine(result))
+    println(calculateSum(listOfFruits))
   }
 }
 
 fun parseLine(line: String): List<Fruit> {
-  return line.split(",")
-    .map {
-      when (it) {
-        "Apple" -> Apple
-        "Cherry" -> Cherry
-        "Banana" -> Banana
-        else -> Empty
-      }
-    }
-    .toList()
+  val fruit = when (line) {
+    "Apple",
+    "Manzana",
+    "Pomme" -> Apple
+    "Cherry" -> Cherry
+    "Banana" -> Banana
+    else -> Empty
+  }
+
+  return listOf(fruit)
 }
 
-fun calculateSum(elements: List<Fruit>): Int {
-  val sum = elements.map { it.price }.sum()
-  val totalDiscount = values()
-    .map { fruits ->
-      fruits to elements.count { it == fruits }
-    }
-    .map {
-      it.first.discount * (it.second / 2)
-    }
-    .sum()
-  return sum - totalDiscount
+fun calculateSum(fruits: List<Fruit>): Int {
+  return fruits.map { it.price }.sum() - calculateDiscount(fruits)
 }
+
+fun calculateDiscount(fruits: List<Fruit>): Int =
+  Fruit.values()
+    .map { fruit -> fruit to fruits.count { it == fruit } }
+    .map { it.first.discount * (it.second / 2) }
+    .sum()
+
+
--- src/test/groovy/com/zopa/katas/FeatureShould.groovy
+++ src/test/groovy/com/zopa/katas/FeatureShould.groovy
@@ -2,10 +2,24 @@ package com.zopa.katas
 
 import spock.lang.Specification
 
+import static com.zopa.katas.Fruit.*
+
 class FeatureShould extends Specification {
 
-  def x() {
+  def "calculate discount"() {
     expect:
-    2 == 1
+    def fruits = [Apple, Cherry, Cherry, Banana, Banana, Banana]
+    def result = FeatureKt.calculateDiscount(fruits)
+
+    result == Cherry.discount + Banana.discount
+  }
+
+  def "calculate sum"() {
+    expect:
+    def fruits = [Apple, Cherry, Cherry, Banana, Banana, Banana]
+    def discount = Cherry.discount + Banana.discount
+    def result = FeatureKt.calculateSum(fruits)
+
+    result == Apple.price + Cherry.price * 2 + Banana.price * 3 - discount
   }
 }
