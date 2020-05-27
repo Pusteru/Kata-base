c1dd507fd15581699e0f80686ab8727a3f598e85
javier.salinas <javier.salinas@zopa.com>
2020-05-27 14:50:51 +0200

    second iteration

--- src/main/kotlin/com/zopa/katas/Feature.kt
+++ src/main/kotlin/com/zopa/katas/Feature.kt
@@ -1,20 +1,37 @@
 package com.zopa.katas
 
 
+val elements: MutableList<Fruit> = mutableListOf()
+
+enum class Fruit(val price: Int, val discount: Int) {
+  Apple(100, 0),
+  Cherry(70, 20),
+  Banana(130, 0)
+}
+
 fun main() {
-  var sum = 0
   while (true) {
     val result = readLine()!!
-    sum += toSum(result)
-    println(sum)
-  }
-}
-fun toSum(fruit: String): Int {
-  return when (fruit) {
-    "Apple" -> 100
-    "Cherry" -> 70
-    "Banana" -> 130
-    else -> 0
+    val fruit = try {
+      Fruit.valueOf(result)
+    } catch (e: Exception) {
+      println(calculateSum(elements))
+      continue
+    }
+    elements.add(fruit)
+    println(calculateSum(elements))
   }
 }
 
+fun calculateSum(elements: List<Fruit>): Int {
+  val sum = elements.map { it.price }.sum()
+  val totalDiscount = Fruit.values()
+    .map { fruits ->
+      fruits to elements.count { it == fruits }
+    }
+    .map {
+      it.first.discount * (it.second / 2)
+    }
+    .sum()
+  return sum - totalDiscount
+}
